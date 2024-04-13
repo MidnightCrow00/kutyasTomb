5243c59af862fa84e358759e96c18c0a3eda2886
MidnightCrow00 <orsolyaalbert1@gmail.com>
2024-04-13 13:20:16 +0200

    first commit

---
+++ adat.js
@@ -0,0 +1,22 @@
+export const kutyakLISTA = [
+    {
+      nev: "Szeder",
+      kor: 6,
+      nem: false,
+      faj:"Szamojéd",
+    },
+    {
+      nev: "Cápa",
+      kor: 4,
+      nem: true,
+      faj:"Belga juhász",
+    },
+    {
+      nev: "Mancs",
+      kor: 3,
+      nem: false,
+      faj:"Ausztrál pásztor",
+    },
+ 
+  ];
+  
